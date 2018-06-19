7b767b18e316722a0ec337dc318752621d7d8f66
EdPutans <edputans@gmail.com>
2018-06-19 14:40:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 }
 
 function deleteFromObjectByKey(object,key){
-  delete object.key
-  return object
+  dobj3 = delete object.key
+  return obj3
 }
+
